4ac611e3da26b75f0c7cf8d62f1f6de2bea0dda8
Zoran Obradović <zockyo@gmail.com>
2019-10-28 22:46:53 +0100

    init

--- README.md
+++ README.md
@@ -1,2 +1,15 @@
 # template-app-vanilla
-The WebShell app template using Vanilla JS
+A Vanilla JS template for WebShell apps 
+
+## Quick Start
+
+1. Copy this repo
+2. Edit the app's manifest and commads in `index.js`
+3. `npm run start` to run the application locally. Prepend `PORT=[number]` to run on a different port than 8080
+4. Go to the [WebShell Sandbox](https://websh.org/sandbox)
+5. In the launcher, open your app from your local url, e.g. `http://localhost:8080`
+6. When ready, publish your app on the internet. GitHub pages is a good place.
+7. Open your app from the launcher on the WebShell sandbox and use it as you please.
+
+## Note
+* Your app must be a single-page app. Navigating to a different page will unload your app.
---
+++ index.js
@@ -0,0 +1,53 @@
+WebShellApp
+  .manifest({
+    "v": 0,
+    "name": "Vanilla Example App",
+    "short_name": "Vanilla",
+    "version": "0.0.1",
+    "icon": "icon.svg",
+    "description": "The Vanilla JS example app for WebShell ",
+    "license": "MIT",
+    "homepage": "https://github.com/websh-org/template-app-vanilla#readme",
+    "repository": "https://github.com/websh-org/template-app-vanilla.git",
+    "issues": "https://github.com/websh-org/template-app-vanilla/issues",
+    "api": {
+      "file": {
+        "new": "text",
+        "open": ["text"],
+        "save": ["text"],
+        "formats": {
+          "text": {
+            "name": "Text File",
+            "extension": "txt",
+            "type": "text/plain",
+            "accept": "text/*"
+          }
+        }
+      }
+    }
+  })
+
+const editor = document.getElementById("editor");
+
+WebShellApp.command("file-new", function () {
+  editor.value = ""
+})
+
+WebShellApp.command("file-open", function ({ format, content, type, extension }) {
+  // you can do different things here based on the type, format and extansion
+
+  if (type === "text/html") {
+    this.throw("file-bad-file", {
+      reason: "This is not a HTML editor."
+    })
+  }
+
+  editor.value = content
+})
+
+WebShellApp.command("file-save", async function ({ format }) {
+  // you can do different things here based on the format 
+  // we dont't really need async/await here, we're just using 
+  // it to demonstrate 
+  return await { content: editor.value };
+})
